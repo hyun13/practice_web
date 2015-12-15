748a576c974c61cd59edf632b4f0a64a11294baa
hyun13 <hyunjung13@yahoo.com>
2015-12-16 06:21:37 +0900

    write parenthesis

--- practice_web/index.php
+++ practice_web/index.php
@@ -3,8 +3,8 @@ include_once 'header.php';
 
 echo "<br /><span class='main'>Welcome";
 
-if ($loggedin) echo " $user, you are logged in.";
-else           echo ' please sign up and/or log in to join in.';
+if ($loggedin) { echo " $user, you are logged in."; }
+else           { echo " please sign up and/or log in to join in."; }
 
 ?>
 
